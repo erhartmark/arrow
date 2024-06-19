3ee366a9cce5c2c237d5f91ed2cd4643b03ffd11
Mark <erhartmark@gmail.com>
2024-06-19 11:30:20 -0400

    14.6

---
+++ index.js
@@ -0,0 +1,14 @@
+const teaOrder = {
+    variety : 'oolong',
+    teaName : 'winter sprout',
+    origin : 'taiwan',
+    price : 12.99,
+    hasCaffeine : true,
+    quantity : 3
+};
+
+// const price = teaOrder.price;
+// const quantity = teaOrder.quantity;
+// const teaName = teaOrder.teaName;
+
+const {price, quantity, teaName} = teaOrder;
